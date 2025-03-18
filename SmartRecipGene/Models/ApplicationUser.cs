eb272889edd647f17d06b45a069de25ec78069c6
diya7090 <diyadodhiawala@gmail.com>
2025-03-19 01:29:44 +0530

    18/3/25

---
+++ SmartRecipGene/Models/ApplicationUser.cs
@@ -0,0 +1,52 @@
+﻿//using Microsoft.AspNetCore.Identity;
+//using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations;
+
+//namespace SmartRecipGene.Models
+//{
+//    public class ApplicationUser : IdentityUser
+//    {
+//        [PersonalData]
+//        [StringLength(100)]
+//        public string Name { get; set; }
+
+
+//        [PersonalData]
+//        [StringLength(1000)]
+//        public string Preferences { get; set; } = "[]";
+//        public List<Recipe> SavedRecipes { get; set; } // User's saved recipes
+//        public ICollection<UserActivity> Activities { get; set; } = new List<UserActivity>(); // ✅ FIXED
+//    }
+//}
+
+
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartRecipGene.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+            SavedRecipes = new List<Recipe>();
+            Activities = new List<UserActivity>();
+            FavoriteRecipes = new List<FavoriteRecipe>();
+            Reviews = new List<Review>();
+        }
+
+        [PersonalData]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [PersonalData]
+        [StringLength(1000)]
+        public string Preferences { get; set; } = "[]";
+
+        public virtual ICollection<Recipe> SavedRecipes { get; set; }
+        public virtual ICollection<UserActivity> Activities { get; set; }
+        public virtual ICollection<FavoriteRecipe> FavoriteRecipes { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ SmartRecipGene/Models/Contact.cs
@@ -0,0 +1,10 @@
+public class Contact
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Subject { get; set; }
+    public string Message { get; set; }
+    public DateTime DateSubmitted { get; set; } = DateTime.UtcNow;
+    public bool IsRead { get; set; } = false;
+}
