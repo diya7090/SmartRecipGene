b7b748d84c922eb5a7fc784188fc6fd46adcea7d
diya7090 <diyadodhiawala@gmail.com>
2025-03-31 10:43:02 +0530

    31/3/25

--- SmartRecipGene/Controllers/HomeController.cs
+++ SmartRecipGene/Controllers/HomeController.cs
@@ -219,7 +219,8 @@ namespace SmartRecipGene.Controllers
                         throw new Exception("Recipe not found in API");
                     }
                 }
-
+var recipeUrl = $"http://localhost:7119/Home/RecipeDetails/{id}";
+                ViewBag.RecipeUrl = recipeUrl;
                 ViewBag.Reviews = reviews;
                 return View(recipeData);
             }
@@ -244,131 +245,6 @@ namespace SmartRecipGene.Controllers
             }
         }
 
-        //private bool ContainsNonVegetarianIngredients(JToken recipe)
-        //{
-        //    var ingredients = recipe["extendedIngredients"]?.ToObject<JArray>();
-        //    if (ingredients == null) return false;
-
-        //    var nonVegKeywords = new[] { "egg", "eggs", "chicken", "beef", "pork", "fish", "mutton",
-        //"shrimp", "crab", "duck", "meat", "bacon", "ham", "turkey", "seafood", "prawn", "mayonnaise" };
-
-        //    return ingredients.Any(ingredient =>
-        //        nonVegKeywords.Any(keyword =>
-        //            ingredient["name"]?.ToString().ToLower().Contains(keyword) == true));
-        //}
-
-        //[HttpGet]
-        //public async Task<IActionResult> SearchRecipes(string query, string cuisine, string mealType, string diet)
-        //{
-        //    var combinedResults = new JArray();
-
-        //    // First, search in database
-        //    var dbQuery = _context.Recipes.AsQueryable();
-
-        //    if (!string.IsNullOrEmpty(query))
-        //    {
-        //        dbQuery = dbQuery.Where(r => r.Title.Contains(query));
-        //    }
-        //    if (!string.IsNullOrEmpty(cuisine))
-        //    {
-        //        dbQuery = dbQuery.Where(r => r.CusineType == cuisine);
-        //    }
-        //    if (!string.IsNullOrEmpty(mealType))
-        //    {
-        //        dbQuery = dbQuery.Where(r => r.MealType == mealType);
-        //    }
-
-        //    var dbRecipes = await dbQuery.ToListAsync();
-        //    foreach (var recipe in dbRecipes)
-        //    {
-        //        var recipeJson = new JObject
-        //        {
-        //            ["id"] = recipe.Id,
-        //            ["title"] = recipe.Title,
-        //            ["image"] = recipe.ImageUrl ?? "",
-        //            ["readyInMinutes"] = recipe.CookingTime,
-        //            ["servings"] = recipe.Servings,
-        //            ["sourceType"] = "database"
-        //        };
-        //        combinedResults.Add(recipeJson);
-        //    }
-
-        //    // Then search in API
-        //    var apiKey = _spoonacularSettings.ApiKey;
-
-        //    string url = $"https://api.spoonacular.com/recipes/complexSearch?apiKey={apiKey}&query={query}&number=100";
-
-        //    if (!string.IsNullOrEmpty(cuisine))
-        //    {
-        //        url += $"&cuisine={cuisine.ToLower()}";
-        //    }
-        //    if (!string.IsNullOrEmpty(mealType))
-        //    {
-        //        url += $"&type={mealType.ToLower()}";
-        //    }
-        //    if (!string.IsNullOrEmpty(diet))
-        //    {
-        //        switch (diet.ToLower())
-        //        {
-        //            case "vegetarian":
-        //                url += "&diet=vegetarian";
-        //                url += "&excludeIngredients=chicken,beef,pork,fish,mutton,shrimp,crab,duck,meat,bacon,ham,turkey,seafood,prawn,egg,eggs,mayonnaise";
-        //                break;
-        //            case "vegan":
-        //                url += "&diet=vegan";
-        //                break;
-        //            case "gluten-free":
-        //                url += "&diet=gluten-free";
-        //                break;
-        //        }
-        //    }
-
-        //    url += "&fillIngredients=true&addRecipeInformation=true";
-
-        //    try
-        //    {
-        //        var response = await _httpClient.GetStringAsync(url);
-        //        var jsonResponse = JObject.Parse(response);
-
-        //        if (jsonResponse.ContainsKey("results") && jsonResponse["results"].HasValues)
-        //        {
-        //            var apiRecipes = jsonResponse["results"].ToObject<JArray>();
-        //            foreach (var recipe in apiRecipes)
-        //            {
-        //                recipe["sourceType"] = "api";
-        //                combinedResults.Add(recipe);
-        //            }
-        //        }
-
-        //        // Apply vegetarian filtering if needed
-        //        if (diet?.ToLower() == "vegetarian")
-        //        {
-        //            combinedResults = new JArray(
-        //                combinedResults.Where(r =>
-        //                    (r["sourceType"].ToString() == "database") ||
-        //                    (r["sourceType"].ToString() == "api" &&
-        //                    r["vegetarian"]?.Value<bool>() == true &&
-        //                    !ContainsNonVegetarianIngredients(r) &&
-        //                    !r["title"].ToString().ToLower().Contains("egg"))
-        //                )
-        //            );
-        //        }
-
-        //        if (!combinedResults.HasValues)
-        //        {
-        //            ViewBag.Message = "No recipes found based on your filters.";
-        //            return View("RecipeResults", new JArray());
-        //        }
-
-        //        return View("RecipeResults", combinedResults);
-        //    }
-        //    catch (HttpRequestException ex)
-        //    {
-        //        Console.WriteLine($"API Error: {ex.Message}");
-        //        ViewBag.Message = "Error fetching recipes from API. Showing database results only.";
-        //        return View("RecipeResults", combinedResults);
-        //    }
-        //}
 
 
         [HttpGet]
@@ -619,129 +495,7 @@ namespace SmartRecipGene.Controllers
             }
             return RedirectToAction("ShoppingList");
         }
-        //[HttpGet]
-        //[Authorize(Roles = "Admin")]
-        //public async Task<IActionResult> AllReviews()
-        //{
-        //    var reviews = await _context.Reviews
-        //        .OrderByDescending(r => r.Id)
-        //        .ToListAsync();
-
-        //    var recipeDetails = new Dictionary<int, string>();
-
-        //    foreach (var review in reviews)
-        //    {
-        //        try
-        //        {
-        //            if (!recipeDetails.ContainsKey(review.RecipeId))  // Only fetch if not already in dictionary
-        //            {
-        //                using (var httpClient = new HttpClient())
-        //                {
-        //                    var apiKey = _spoonacularSettings.ApiKey;
-
-        //                    string apiUrl = $"https://api.spoonacular.com/recipes/{review.RecipeId}/information?apiKey={apiKey}";
-        //                    var response = await httpClient.GetAsync(apiUrl);
-
-        //                    //if (response.IsSuccessStatusCode)
-        //                    //{
-        //                    //    string jsonResponse = await response.Content.ReadAsStringAsync();
-        //                    //    var recipeData = JObject.Parse(jsonResponse);
-        //                    //    recipeDetails[review.RecipeId] = recipeData["title"]?.ToString() ?? $"Recipe {review.RecipeId}";
-        //                    //}
-        //                    //else
-        //                    //{
-        //                    //    recipeDetails[review.RecipeId] = $"Recipe {review.RecipeId}";
-        //                    //}
-        //                }
-        //            }
-        //        }
-        //        catch (Exception)
-        //        {
-        //            recipeDetails[review.RecipeId] = $"Recipe {review.RecipeId}";
-        //        }
-        //    }
-
-        //    ViewBag.RecipeDetails = recipeDetails;
-        //    return View("~/Views/Admin/AllReviews.cshtml", reviews);
-        //}
-        // [HttpGet]--------final recipe
-        //[Authorize(Roles = "Admin")]
-        //public async Task<IActionResult> AllReviews()
-        //{
-        //    var reviews = await _context.Reviews
-        //        .OrderByDescending(r => r.Id)
-        //        .ToListAsync();
-
-        //    var recipeDetails = new Dictionary<int, string>();
-
-        //    foreach (var review in reviews)
-        //    {
-        //        try
-        //        {
-        //            if (!recipeDetails.ContainsKey(review.RecipeId))
-        //            {
-        //                // First check if we have the recipe in our database
-        //                var dbRecipe = await _context.Recipes.FindAsync(review.RecipeId);
-        //                if (dbRecipe != null && !string.IsNullOrEmpty(dbRecipe.Title))
-        //                {
-        //                    recipeDetails[review.RecipeId] = dbRecipe.Title;
-        //                    continue; // Skip API call if we found it in database
-        //                }
-
-        //                using (var httpClient = _httpClient)
-        //                {
-        //                    var apiKey = _spoonacularSettings.ApiKey;
-        //                    string apiUrl = $"https://api.spoonacular.com/recipes/{review.RecipeId}/information?apiKey={apiKey}";
-        //                    var response = await httpClient.GetAsync(apiUrl);
-
-        //                    if (response.IsSuccessStatusCode)
-        //                    {
-        //                        string jsonResponse = await response.Content.ReadAsStringAsync();
-        //                        var recipeData = JObject.Parse(jsonResponse);
-        //                        string title = recipeData["title"]?.ToString();
-
-        //                        if (!string.IsNullOrEmpty(title))
-        //                        {
-        //                            recipeDetails[review.RecipeId] = title;
-
-        //                            // Update or add to database
-        //                            if (dbRecipe == null)
-        //                            {
-        //                                _context.Recipes.Add(new Recipe
-        //                                {
-        //                                    Id = review.RecipeId,
-        //                                    Title = title
-        //                                });
-        //                            }
-        //                            else
-        //                            {
-        //                                dbRecipe.Title = title;
-        //                                _context.Recipes.Update(dbRecipe);
-        //                            }
-        //                            await _context.SaveChangesAsync();
-        //                        }
-        //                        else
-        //                        {
-        //                            recipeDetails[review.RecipeId] = $"Recipe {review.RecipeId}";
-        //                        }
-        //                    }
-        //                    else
-        //                    {
-        //                        recipeDetails[review.RecipeId] = $"Recipe {review.RecipeId}";
-        //                    }
-        //                }
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            _logger.LogError(ex, $"Error fetching recipe details for ID {review.RecipeId}");
-        //            recipeDetails[review.RecipeId] = $"Recipe {review.RecipeId}";
-        //        }
-        //    }
-
-        //    ViewBag.RecipeDetails = recipeDetails;
-        //    return View("~/Views/Admin/AllReviews.cshtml", reviews);
-        //}
+       
         [HttpGet]
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> AllReviews()
@@ -1050,6 +804,7 @@ public async Task<IActionResult> GetSpellingSuggestions(string query)
             // }
             // return Json(new { success = false, message = "Please check your input and try again." });
         }
+
 
         public IActionResult Privacy()
         {
